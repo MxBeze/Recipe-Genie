b177642cd2748128e58475c871d51f9c0b1ee3ae
MxBeze <MxBeze@users.noreply.github.com>
2024-04-27 00:16:35 -0600

    Added Axios, Typewriter and basic JS

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//alert("Hello");//
+function getRecipe(event){
+    event.preventDefault();
+
+    let recipeElement = document.querySelector ("#recipe")
+    recipeElement.innerHTML = "The Poem";
+
+    new Typewriter("#recipe", {
+        strings: [recipeElement.innerHTML],
+        autoStart: true,
+        cursor: null,
+        delay: 100,
+      });
+}
+
+
+let recipeForm = document.querySelector ("#recipe-form")
+recipeForm.addEventListener("submit", getRecipe);
